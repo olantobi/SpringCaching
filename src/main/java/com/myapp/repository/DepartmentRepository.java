7af9e74a56e1d6b9062cb14ac35f799a7af77777
Iftekhar Ahmed Khan <iftekharkhan09@users.noreply.github.com>
2017-10-09 23:37:15 +0530

    Added changes.

--- src/main/java/com/myapp/repository/DepartmentRepository.java
+++ src/main/java/com/myapp/repository/DepartmentRepository.java
@@ -4,8 +4,11 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.myapp.beans.Department;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 	
 	@Cacheable(value="departments")
